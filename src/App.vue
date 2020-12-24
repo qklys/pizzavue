72c41b7caa3c5d1fb0d50629dfa4d073152c2a7a
Сергей Филатов <modern443@gmail.com>
2020-12-24 17:59:07 +0300

    pizza size

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Parent: {{pizzaName}}</h1>
+   <app-pizza v-bind:pizzaName='pizzaName' v-bind:pizzaPrice='pizzaPrice' v-bind:pizzaSize='pizzaSize'>
+       
+
+     <h1>Мы рады вам предложить!</h1>
+     <h2 slot='n1'></h2>
+   </app-pizza>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pizza from './components/Pizza.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data(){
+    return {
+      pizzaName: 'Пицца от школы программистов',
+      pizzaPrice: 600,
+      pizzaSize: '60cm',
+    }
+  },
   components: {
-    HelloWorld
-  }
+    appPizza: Pizza
+  } 
 }
 </script>
 
---
+++ src/components/Pizza.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2> Pizza Name: {{pizzaName}}</h2>
+        <p>Price: {{pizzaPrice}}</p>
+        <p>Size: {{pizzaSize}}</p>
+                <div>
+            <slot name='n1'>
+            </slot>
+            <slot>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pizzaName','pizzaPrice', 'pizzaSize'],    
+}
+</script>
